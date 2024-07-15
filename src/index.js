9bc5c01b56516fda55322a0e11b726d1205fce06
daniel cooder <danielcooder@gmail.com>
2024-07-15 18:27:11 -0300

    perf: parametro de rotas

--- src/index.js
+++ src/index.js
@@ -2,10 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const professores = [
 
-    res.send('Página inicial');
+    { id: 1, nome: 'João Alberto', stack: 'Literatura'},
 
+    { id: 2, nome: 'Antônio Silva', stack: 'Artes'},
+
+    { id: 3, nome: 'Maria Oliveira', stack: 'Matematica'},
+
+    { id: 4, nome: 'Mauricio de Souza', stack: 'Biologia' },
+
+    { id: 5, nome: 'Sergiane', stack: 'Algebra' },
+
+    { id: 6, nome: 'Paula', stack: 'Educação financeira' },
+
+    { id: 7, nome: 'Ana Caetano', stack: 'Historia' }
+
+];
+
+//localhost:3000/professores/:id
+app.get('/professores', (req, res) => {
+    res.send(professores);
 });
+
+//localhost:3000/professores/3
+app.get('/professores/:id', (req, res) => {
+   const professorEncontrado = professores.find((professor) => {
+    return professor.id === Number(req.params.id)
+   });
+
+    res.send(professorEncontrado);   
+
+}); 
+
 
 app.listen(3000);
